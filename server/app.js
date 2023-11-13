e71b7bac4c0c917649ff34c7e8318bf731bbf09d
ShubhamGhosh1993 <shubham.ghoshofficial@gmail.com>
2023-11-13 23:47:44 +0530

    Routers handling

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL;
 const apiKey = process.env.API_KEY;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome");
-});
+const auth=require("./routes/auth");
+
+app.use("/", auth);
 
 app.listen(port, () => {
   console.log(`Server is running at port : ${port}`);
